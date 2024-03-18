56d8e95f9154d1fa417f53963123a155db0d2f05
112b16718 <161404839+112b16718@users.noreply.github.com>
2024-03-18 21:10:21 +0800

    Update jpa06.java

--- jpa06.java
+++ jpa06.java
@@ -1,4 +1,3 @@
-package lab03;
 import java.util.Scanner;
 public class jpa06 {
     public static void main(String[] args) {
@@ -9,7 +8,12 @@ public class jpa06 {
         if (n1==999){
             break;
         }
-        System.out.println(Math.pow(n2,n1));
+        int t=1;
+        while (n2!=0){
+            t*=n1;
+            n2--;
+        }
+        System.out.println(t);
         
         }
         a.close();
